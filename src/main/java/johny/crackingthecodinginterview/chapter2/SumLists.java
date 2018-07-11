719c3ca57f7558b9f90c1f27fb205f8edf37f8ef
Johny Thomas <johnythomas007@gmail.com>
2018-07-11 22:58:38 +0530

    Add SumLists

--- src/main/java/johny/crackingthecodinginterview/chapter2/SumLists.java
+++ src/main/java/johny/crackingthecodinginterview/chapter2/SumLists.java
@@ -4,7 +4,7 @@ import johny.crackingthecodinginterview.utils.Node;
 
 public class SumLists {
 
-    public Node addList(Node l1, Node l2, int carry) {
+    public Node addReverseList(Node l1, Node l2, int carry) {
         if (l1 == null && l2 == null && carry == 0) {
             return null;
         }
@@ -21,7 +21,7 @@ public class SumLists {
         result.setData(value % 10);
 
         if (l1 != null || l2 != null) {
-            Node more = addList(
+            Node more = addReverseList(
                     l1 == null ? null : l1.getNext(),
                     l2 == null ? null : l2.getNext(),
                     value >= 10 ? 1 : 0);
@@ -30,4 +30,76 @@ public class SumLists {
 
         return result;
     }
+
+    public Node addList(Node l1, Node l2) {
+        int len1 = length(l1);
+        int len2 = length(l2);
+
+        if (len1 < len2) {
+            l1 = padList(l1, len2 - len1);
+        } else {
+            l2 = padList(l2, len1 - len2);
+        }
+
+        PartialSum sum = addListHelper(l1, l2);
+
+        if (sum.carry == 0) {
+            return sum.sum;
+        } else {
+            Node result = insertBefore(sum.sum, sum.carry);
+            return result;
+        }
+    }
+
+    private PartialSum addListHelper(Node l1, Node l2) {
+        if (l1 == null && l2 == null) {
+            PartialSum sum = new PartialSum();
+            return sum;
+        }
+
+        PartialSum sum = addListHelper(l1.getNext(), l2.getNext());
+
+        int val = sum.carry + l1.getData() + l2.getData();
+
+        Node fullResult = insertBefore(sum.sum, val % 10);
+
+        sum.sum = fullResult;
+        sum.carry = val / 10;
+        return sum;
+    }
+
+    private int length(Node list) {
+        Node runner = list;
+        int len = 0;
+        while (runner != null) {
+            runner = runner.getNext();
+            len++;
+        }
+        return len;
+    }
+
+    private Node padList(Node l, int padding) {
+        Node head = l;
+        for (int i = 0; i < padding; i++) {
+            head = insertBefore(head, 0);
+        }
+        return head;
+    }
+
+    private Node insertBefore(Node list, int data) {
+        Node node = new Node();
+        node.setData(data);
+        if (list != null) {
+            node.setNext(list);
+        }
+        return node;
+    }
+
 }
+
+class PartialSum {
+    public Node sum = null;
+    public int carry = 0;
+}
+
+
--- src/test/java/johny/crackingthecodinginterview/chapter2/SumListsTest.java
+++ src/test/java/johny/crackingthecodinginterview/chapter2/SumListsTest.java
@@ -16,26 +16,67 @@ public class SumListsTest {
     }
 
     @Test
-    public void testAddListsForEmptyLists() {
-        assertThat(this.sumLists.addList(null, null, 0)).isNull();
+    public void testAddReverseListsForEmptyLists() {
+        assertThat(this.sumLists.addReverseList(null, null, 0)).isNull();
     }
 
     @Test
-    public void testAddListsForSameSizeList() {
+    public void testAddReverseListsForSameSizeList() {
         Node l1 = new Node(1, new Node(2, new Node(3, null)));
         Node l2 = new Node(1, new Node(2, new Node(5, null)));
 
-        Node resultNode = this.sumLists.addList(l1, l2, 0);
+        Node resultNode = this.sumLists.addReverseList(l1, l2, 0);
         assertNode(resultNode, new int[]{2, 4, 8});
     }
 
     @Test
-    public void testAddListsForSumWithCarry() {
+    public void testAddReverseListsForSumWithCarry() {
         Node l1 = new Node(9, new Node(5, new Node(4, null)));
         Node l2 = new Node(8, new Node(7, new Node(6, null)));
 
-        Node resultNode = this.sumLists.addList(l1, l2, 0);
+        Node resultNode = this.sumLists.addReverseList(l1, l2, 0);
         assertNode(resultNode, new int[]{7, 3, 1, 1});
+    }
+
+    @Test
+    public void testAddReverseListsForUnequalLengthLists() {
+        Node l1 = new Node(9, new Node(5, new Node(4, new Node(5, null))));
+        Node l2 = new Node(8, new Node(7, new Node(6, null)));
+
+        Node resultNode = this.sumLists.addReverseList(l1, l2, 0);
+        assertNode(resultNode, new int[]{7, 3, 1, 6});
+    }
+
+    @Test
+    public void testAddListsForEmptyLists() {
+        assertThat(this.sumLists.addList(null, null)).isNull();
+    }
+
+    @Test
+    public void testAddListsForSameSizeList() {
+        Node l1 = new Node(3, new Node(2, new Node(1, null)));
+        Node l2 = new Node(5, new Node(2, new Node(1, null)));
+
+        Node resultNode = this.sumLists.addList(l1, l2);
+        assertNode(resultNode, new int[]{8, 4, 2});
+    }
+
+    @Test
+    public void testAddListsForSumWithCarry() {
+        Node l1 = new Node(4, new Node(5, new Node(9, null)));
+        Node l2 = new Node(6, new Node(7, new Node(8, null)));
+
+        Node resultNode = this.sumLists.addList(l1, l2);
+        assertNode(resultNode, new int[]{1, 1, 3, 7});
+    }
+
+    @Test
+    public void testAddListsForUnequalLengthLists() {
+        Node l1 = new Node(5, new Node(4, new Node(5, new Node(9, null))));
+        Node l2 = new Node(6, new Node(7, new Node(8, null)));
+
+        Node resultNode = this.sumLists.addList(l1, l2);
+        assertNode(resultNode, new int[]{6, 1, 3, 7});
     }
 
     public void assertNode(Node node, int[] expectedValue) {
